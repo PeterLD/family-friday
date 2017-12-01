aa0b58b5edfb724affab81b1fcfb894477cde839
Peter Lyle-Dugas <p.lyled@gmail.com>
2017-12-01 14:57:21 -0800

    Initialize React app with Semantic-UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header as='h1'>Family Friday</Header>
+      </Container>
+    );
+  }
+}
+
+export default App;

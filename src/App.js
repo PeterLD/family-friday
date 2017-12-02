671653c2462e29f0160f696ac6c74bcf990aed33
Peter Lyle-Dugas <p.lyled@gmail.com>
2017-12-01 16:59:46 -0800

    Create layout MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon, Input, List } from 'semantic-ui-react';
 
 class App extends Component {
+  state = {
+    employeeGroups: [
+      ['sadfasdf', 'fdasdfd', 'fdafda', 'asdfdsfgfshcxx', 'sadfkljas'],
+      ['ncdkjhgdksb', 'coijrwklj', 'fasjodfkjno', 'nbasdfkjal', 'asdfljas'],
+      ['sadkfnlad', 'aosnejfn', 'bejwfladfjks', 'bksadjfla', 'naldfjks'],
+      ['asdfkljs', 'asdflkasl', 'elkfjasonb']
+    ]
+  }
+
   render() {
     return (
-      <Container>
+      <Container style={{ marginTop: '2em' }}>
         <Header as='h1'>Family Friday</Header>
+        <Input
+          icon={<Icon name='add user' inverted circular link />}
+          placeholder='Add employee...' />
+        <Button content='Shuffle' icon='random' labelPosition='right' primary />
+        <Grid columns={3} celled>
+          {this.state.employeeGroups.map((group, i) => (
+            <Grid.Column key={i}>
+              <Header as='h2'>Group {i + 1}</Header>
+              <List>
+                {group.map((employee, j) => (
+                  <List.Item key={j}>{employee}</List.Item>
+                ))}
+              </List>
+            </Grid.Column>
+          ))}
+        </Grid>
       </Container>
     );
   }
